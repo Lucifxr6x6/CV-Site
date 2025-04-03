435fd4d61cd92ed1b9efd95194c992057fa25abb
Lucifxr6x6 <103212710+Lucifxr6x6@users.noreply.github.com>
2025-04-03 16:40:45 +0300

    Add files via upload

---
+++ Infrastructure/Terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "folder_id" {
+  description = "The ID of the Yandex Cloud folder"
+  type        = string
+  default     = ""
+}
+
+variable "token" {
+  description = "Token"
+  type        = string
+  default     = ""
+}
+
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+  default     = ""
+}
